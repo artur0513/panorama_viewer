c76aa2379e77abc44c59fcf60c483bd6c200ca80
artur0513 <artur.salaridze@yandex.ru>
2023-02-25 20:15:56 +0300

    first commit

---
+++ cubemap2panorama.cpp
@@ -0,0 +1,135 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	//OpenGL has its own cubemap implementation (and i guess it is significatly faster), but im to lazy to use it
+	Clock clock;
+	Time time;
+
+
+	// getting path to exe
+	string exe_path = argv[0];
+	exe_path.erase(exe_path.rfind("\\") + 1);
+	ofstream fout(exe_path + "output.txt");
+
+
+	// checking recieved arguements
+	if (argc != 7) {
+		fout << "invalid number of arguements. You should drag&drop 6 images on .exe file" << endl;
+		fout.close();
+		return EXIT_FAILURE;
+	}
+	fout << "recivied arguements: " << endl;
+	for (int i = 0; i < argc; i++) {
+		fout << "#" + to_string(i) + ": " << argv[i] << endl;
+	}
+
+
+	// sort recivied images by a number after symbol "#"
+	vector<string> path(6);
+	for (int i = 1; i <= 6; i++) {
+		path[i - 1] = argv[i];
+
+		if (path[i - 1].find(".") == string::npos || path[i - 1].find("#") == 0) {
+			fout << "invalid input file name format. name shoud be something like ***1.tga. no \".\" symbol found or it is first symbol in name" << endl;
+			fout.close();
+			return EXIT_FAILURE;
+		}
+		char check = path[i - 1][path[i - 1].find(".") - 1];
+		if (check < 49 || check > 54) {
+			fout << "invalid input file name format. name shoud be something like ***1.tga. bad symbol before \".\"." << endl;
+			fout.close();
+			return EXIT_FAILURE;
+		}
+	}
+	sort(path.begin(), path.end(), [](string a, string b) {return a[a.find(".") - 1] < b[b.find(".") - 1]; } );
+
+
+	// reading output file name and size of output texture
+	ifstream fin(exe_path + "input.txt");
+	string temp, output_filename = "output.tga";
+	unsigned output_y = 2048; // Height of the ouput texture
+	if (fin.is_open()) {
+		getline(fin, temp);
+		output_y = stoi(temp);
+		getline(fin, output_filename);
+	}
+	fin.close();
+
+
+	// creating output rendertexture
+	RenderTexture output;
+	if (!output.create(Vector2u(output_y * 2, output_y))) {
+		fout << "unable to create output rendertexture" << endl;
+		fout.close();
+		return EXIT_FAILURE;
+	}
+	output.clear(Color(0, 0, 0, 255));
+	fout << "loading parametrs and creating output texture in milliseconds: " << to_string(clock.getElapsedTime().asMilliseconds()) << endl;
+
+
+	// loading shader
+	time = clock.getElapsedTime();
+	Shader c2p;
+	if (!c2p.loadFromFile(exe_path + "c2p.txt", Shader::Fragment)) {
+		fout << "unable to load shader from c2p.txt" << endl;
+		fout.close();
+		return EXIT_FAILURE;
+	}
+	fout << "shader compiling time in milliseconds: " << to_string((clock.getElapsedTime() - time).asMilliseconds()) << endl;
+
+
+	// loading textures and passing them in shader
+	time = clock.getElapsedTime();
+	vector<Texture*> cubemap;
+	for (int i = 0; i < 6; i++) {
+		Texture* t = new Texture();
+		if (!t->loadFromFile(path[i])) {
+			fout << "error loading cubemap texture " << path[i] << endl;
+			fout.close();
+			return EXIT_FAILURE;
+		};
+		c2p.setUniform("cubemap" + to_string(i), *t);
+		cubemap.push_back(t);
+	}
+	fout << "loading textures and passing them to shader time in milliseconds: " << to_string((clock.getElapsedTime() - time).asMilliseconds()) << endl;
+
+
+	// rendering panorama
+	time = clock.getElapsedTime();
+	Sprite s;
+	s.setTexture(output.getTexture());
+	s.setTextureRect(IntRect(Vector2i(0, 0), Vector2i(output_y * 2, output_y)));
+	output.draw(s, &c2p);
+	fout << "creating panorama time in milliseconds: " << to_string((clock.getElapsedTime() - time).asMilliseconds()) << endl;
+
+
+	// saving result into a file
+	time = clock.getElapsedTime();
+	if (!output.getTexture().copyToImage().saveToFile(exe_path + output_filename)) {
+		fout << "can`t save output image to file" << endl;
+		fout.close();
+		return EXIT_FAILURE;
+	}
+	fout << "saving image to file time in milliseconds: " << to_string((clock.getElapsedTime() - time).asMilliseconds()) << endl;
+
+
+	// some info printing into file
+	string max_size = to_string(Texture::getMaximumSize());
+	fout << "total time in milliseconds: " << to_string(clock.getElapsedTime().asMilliseconds()) << endl;
+	fout << "maximum texture size: " << max_size << "x" << max_size << endl;
+
+
+	// free memory, close file
+	fout.close();
+	for (int i = 0; i < 6; i++) {
+		delete cubemap[i];
+	}
+	return EXIT_SUCCESS;
+}
---
+++ panoramaviewer.cpp
@@ -0,0 +1,130 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <SFML/Graphics.hpp>
+#include <cmath>
+
+using namespace sf;
+using namespace std;
+
+const double PI = 3.14159265359;
+
+int main(int argc, char* argv[])
+{
+	// getting path to exe
+	string exe_path = argv[0];
+	exe_path.erase(exe_path.rfind("\\") + 1);
+	ofstream fout(exe_path + "output.txt");
+	if (argc != 2) {
+		fout << "invalid number of arguements. You should drag&drop 1 panoramic image on .exe file" << endl;
+		fout.close();
+		return EXIT_FAILURE;
+	}
+
+	Texture panorama;
+	if (!panorama.loadFromFile(argv[1])) {
+		fout << "error loading panorama image " << argv[1] << endl;
+		fout.close();
+		return EXIT_FAILURE;
+	};
+	panorama.setSmooth(true);
+
+	Vector2f screen_res = Vector2f(VideoMode::getDesktopMode().size.x, VideoMode::getDesktopMode().size.y);
+	float aspect_ratio = screen_res.x / screen_res.y;
+	RenderWindow window(VideoMode(VideoMode::getDesktopMode()), "panorama viewer", Style::Fullscreen);
+	window.setFramerateLimit(60);
+
+	Vector2f fov;
+	fov.y = PI / 2.0;
+	fov.x = atanf(tanf(fov.y * 0.5f) * aspect_ratio) * 2.0;
+	Vector2f sphere_coords = Vector2f(0.f, 0.0f);
+
+	Shader shader;
+	if (!shader.loadFromFile(exe_path + "pano.txt", Shader::Fragment)) {
+		fout << "error loading shader " << exe_path + "pano.txt" << endl;
+		fout.close();
+		return EXIT_FAILURE;
+	}
+	shader.setUniform("panorama", panorama);
+	shader.setUniform("screen_res", screen_res);
+
+	Sprite s;
+	s.setTexture(panorama);
+	s.setTextureRect(IntRect(Vector2i(0, 0), Vector2i(screen_res)));
+
+	bool mouse_button_pressed = false, fullscreen = true, texture_smooth = true;
+	Vector2i prev_mpos = Mouse::getPosition();
+	Vector2f prev_sphere_coords = sphere_coords;
+	while (window.isOpen()) {
+		shader.setUniform("sphere_coords", sphere_coords);
+		shader.setUniform("fov", fov);
+
+		Event event;
+		while (window.pollEvent(event)) {
+
+			if (event.type == sf::Event::KeyPressed) {
+				if (event.key.code == sf::Keyboard::Escape)
+					window.close();
+
+				if (event.key.code == sf::Keyboard::F11) {
+					fullscreen = !fullscreen;
+					if (fullscreen) {
+						window.create(VideoMode(VideoMode::getDesktopMode()), "panorama viewer", Style::Fullscreen);
+						screen_res = Vector2f(float(window.getSize().x), float(window.getSize().y));
+						aspect_ratio = screen_res.x / screen_res.y;
+						shader.setUniform("screen_res", screen_res);
+						fov.x = atanf(tanf(fov.y * 0.5f) * aspect_ratio) * 2.0;
+					}
+					else
+						window.create(VideoMode(VideoMode::getDesktopMode()), "panorama viewer");
+				}
+
+				if (event.key.code == sf::Keyboard::Space) {
+					texture_smooth = !texture_smooth;
+					panorama.setSmooth(texture_smooth);
+				}
+			}
+
+			if (event.type == sf::Event::Resized) {
+				screen_res = Vector2f(float(window.getSize().x), float(window.getSize().y));
+				aspect_ratio = screen_res.x / screen_res.y;
+				shader.setUniform("screen_res", screen_res);
+				fov.x = atanf(tanf(fov.y * 0.5f) * aspect_ratio) * 2.0;
+			}
+
+			if (event.type == sf::Event::Closed)
+				window.close();
+
+			if (event.type == sf::Event::MouseButtonPressed)
+			{
+				mouse_button_pressed = true;
+				prev_mpos = Mouse::getPosition();
+				prev_sphere_coords = sphere_coords;
+			}
+
+			if (event.type == sf::Event::MouseButtonReleased)
+				mouse_button_pressed = false;
+
+			if (event.type == sf::Event::MouseWheelScrolled) {
+				fov.y *= powf(1.04, -event.mouseWheelScroll.delta);
+				fov.x = atanf(tanf(fov.y * 0.5f) * aspect_ratio) * 2.0;
+				if (fov.x > PI * 0.95) {
+					fov.x = PI * 0.95;
+					fov.y = atanf(tanf(fov.x * 0.5f) / aspect_ratio) * 2.0;
+				}
+			}
+		}
+
+		if (mouse_button_pressed) {
+			sphere_coords.x = prev_sphere_coords.x - float(Mouse::getPosition().x - prev_mpos.x) / float(screen_res.x) * fov.x * 1.2;
+			sphere_coords.y = prev_sphere_coords.y - float(prev_mpos.y - Mouse::getPosition().y) / float(screen_res.y) * fov.y;
+
+			sphere_coords.y = clamp(sphere_coords.y, float(- PI * 0.5), float(PI * 0.5));
+		}
+
+		window.clear(Color(0, 0, 0, 255));
+		window.draw(s, &shader);
+		window.display();
+		
+	}
+}
